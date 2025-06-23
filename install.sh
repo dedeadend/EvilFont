509057ea19057e44f997a0d47a1f632070db4a10
DEADEND <107865626+evil22live@users.noreply.github.com>
2025-06-23 16:20:37 +0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,42 @@
+##################################
+# Magisk Module Installer Script #
+##################################
+
+SKIPMOUNT=false
+
+PROPFILE=false
+
+POSTFSDATA=false
+
+LATESTARTSERVICE=false
+
+print_modname() {
+    ui_print "                               "
+    ui_print "_______________________________"
+    ui_print "                               "
+    ui_print "       E v i l   F o n t       "  
+    ui_print "_______________________________"
+    ui_print "                               "
+}
+
+on_install() {
+    ui_print "                               "
+    ui_print "-------------------------------"
+    ui_print "                               "
+    ui_print "  - Extracting module files    "
+    ui_print "                               "
+    ui_print "  - Adding Fonts ...           "
+    ui_print "                               "
+    ui_print "  [NOTE] Don't Hide Google Apps from Magisk"
+    ui_print "  To work the Module in Google System Apps"
+    ui_print "                               "
+    ui_print "  Created by : evil22live      "
+    ui_print "                               "
+    ui_print "-------------------------------"
+    ui_print "                               "
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+
+set_permissions() {
+  set_perm_recursive $MODPATH 0 0 0755 0644
+}
